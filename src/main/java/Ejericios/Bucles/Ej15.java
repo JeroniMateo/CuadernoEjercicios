1861a0e25e39c51422ea0d032521b730b232b86f
Jeroni <jmateo@cifpfbmoll.eu>
2021-04-26 16:34:43 +0200

    15 Resuelto Faltan 16,17

--- src/main/java/Ejericios/Bucles/Ej15.java
+++ src/main/java/Ejericios/Bucles/Ej15.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class Ej15 {
     public static void main(String[] args) {
         int adivina=0;
-        int num = 0;
-        int vidas=3;
+        int num ;
+        int vidas=5;
 
+        System.out.println("Escribe un numero");
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
 
-        while (!(num == adivina || vidas ==0));{
+        while (num != adivina && vidas !=0){
 
             adivina = (int) (100*Math.random()+1);
 
 
             System.out.println("Escribe un numero");
-            Scanner scanner = new Scanner(System.in);
+             scanner = new Scanner(System.in);
              num = scanner.nextInt();
-
-
-             vidas = vidas -1;
+             vidas -= 1;
 
         }
         if (vidas ==0){
-            System.out.println("Has perdido");
+            System.out.println("Has perdido, el numero era" + adivina );
         }else if (num ==adivina){
             System.out.println("Ganaste");
         }
