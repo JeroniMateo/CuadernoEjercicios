ce7062f651aefe1fd76bdb52666c946a902b9e77
Jeroni <jmateo@cifpfbmoll.eu>
2021-06-06 11:34:11 +0200

    Entrega

--- src/main/java/Ejericios/Arrays/Ej17.java
+++ src/main/java/Ejericios/Arrays/Ej17.java
@@ -6,6 +6,7 @@ public class Ej17 {
     public static void main(String[] args) {
         int i;
         int[] numeros = new int[5];
+        int[] invertido= new int[5];
 
         Scanner teclado = new Scanner(System.in);
 
@@ -25,6 +26,17 @@ public class Ej17 {
 
         for(int j = 0; i < numeros.length; j++) {
             unionString += numeros[j];
+        }
+
+        numeros = invertido;
+
+        for( i=0;i<invertido.length;i++){
+            invertido[i] = invertido[invertido.length-1-i];
+        }
+        if (numeros == invertido ){
+            System.out.println("Es capicua");
+        }else if (numeros != invertido){
+            System.out.println("No es capicua");
         }
 
 
